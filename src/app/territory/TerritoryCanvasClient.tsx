baed56546d84d0bbf9f6a0320551edead5180e9c
nkuchenov-hash <nkuchenov@gmail.com>
2026-08-23 11:50:28 +0300

    Switch territory page to adaptive high resolution globe

--- src/app/territory/TerritoryCanvasClient.tsx
+++ src/app/territory/TerritoryCanvasClient.tsx
@@ -3,7 +3,7 @@
 import dynamic from 'next/dynamic';
 
 const HistoricalTerritoryGlobe = dynamic(
-  () => import('./HistoricalTerritoryGlobeWebGLV2').then((m) => m.HistoricalTerritoryGlobeWebGLV2),
+  () => import('./HistoricalTerritoryGlobeWebGLV3').then((m) => m.HistoricalTerritoryGlobeWebGLV3),
   { ssr: false }
 );
 
