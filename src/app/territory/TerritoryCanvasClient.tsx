a9efae16f5c6200e6f043181601c2e7924ec128d
nkuchenov-hash <nkuchenov@gmail.com>
2026-08-23 03:14:49 +0300

    Switch territory page to smooth canvas globe v2

--- src/app/territory/TerritoryCanvasClient.tsx
+++ src/app/territory/TerritoryCanvasClient.tsx
@@ -3,7 +3,7 @@
 import dynamic from 'next/dynamic';
 
 const HistoricalTerritoryGlobeCanvas = dynamic(
-  () => import('./HistoricalTerritoryGlobeCanvasRuntime').then((m) => m.HistoricalTerritoryGlobeCanvasRuntime),
+  () => import('./HistoricalTerritoryGlobeCanvasV2').then((m) => m.HistoricalTerritoryGlobeCanvasV2),
   { ssr: false }
 );
 
