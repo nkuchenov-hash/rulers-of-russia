e6ac8291c02a716ded08e8e64ceec029a3760ffd
nkuchenov-hash <nkuchenov@gmail.com>
2026-08-25 15:34:02 +0300

    Switch territory page to optimized WebGL V9 renderer

--- src/app/territory/TerritoryCanvasClient.tsx
+++ src/app/territory/TerritoryCanvasClient.tsx
@@ -3,7 +3,7 @@
 import dynamic from 'next/dynamic';
 
 const HistoricalTerritoryGlobe = dynamic(
-  () => import('./HistoricalTerritoryGlobeCanvasRuntime').then((m) => m.HistoricalTerritoryGlobeCanvasRuntime),
+  () => import('./HistoricalTerritoryGlobeWebGLV9').then((m) => m.HistoricalTerritoryGlobeWebGLV9),
   { ssr: false }
 );
 
