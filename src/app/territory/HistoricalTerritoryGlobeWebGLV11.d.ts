f7159a08052aa23e88fcccde30d4e62b830a3239
nkuchenov-hash <nkuchenov@gmail.com>
2026-08-25 18:38:31 +0300

    Add V11 renderer type declaration

---
+++ src/app/territory/HistoricalTerritoryGlobeWebGLV11.d.ts
@@ -0,0 +1,7 @@
+import type { ComponentType } from 'react';
+
+export interface HistoricalTerritoryGlobeWebGLV11Props {
+  initialYear?: number;
+}
+
+export const HistoricalTerritoryGlobeWebGLV11: ComponentType<HistoricalTerritoryGlobeWebGLV11Props>;
